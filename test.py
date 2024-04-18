be98557e2f04c37fae68fb7bb49b35ba1120a3ba
幻16 <1543240048@qq.com>
2024-04-18 20:52:48 +0800

    测试2

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import timeit
 
 with tf.device('/cpu:0'):
-    cpu_a = tf.random.normal([100000, 10000])
-    cpu_b = tf.random.normal([10000, 20000])
+    cpu_a = tf.random.normal([10000, 1000])
+    cpu_b = tf.random.normal([1000, 2000])
     print(cpu_a.device, cpu_b.device)
 with tf.device('/gpu:0'):
-    gpu_a = tf.random.normal([100000, 10000])
-    gpu_b = tf.random.normal([10000, 20000])
+    gpu_a = tf.random.normal([10000, 1000])
+    gpu_b = tf.random.normal([1000, 2000])
     print(gpu_a.device, gpu_b.device)
 def cpu_run():
     with tf.device('/cpu:0'):
@@ -18,11 +18,11 @@ def gpu_run():
         c = tf.matmul(gpu_a, gpu_b)
     return c
 # warm up
-cpu_time = timeit.timeit(cpu_run, number=100)
-gpu_time = timeit.timeit(gpu_run, number=100)
+cpu_time = timeit.timeit(cpu_run, number=10)
+gpu_time = timeit.timeit(gpu_run, number=10)
 print('warmup:', cpu_time, gpu_time)
 
-cpu_time = timeit.timeit(cpu_run, number=100)
-gpu_time = timeit.timeit(gpu_run, number=100)
+cpu_time = timeit.timeit(cpu_run, number=10)
+gpu_time = timeit.timeit(gpu_run, number=10)
 print('run time:', cpu_time, gpu_time)
 
